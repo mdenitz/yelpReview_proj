de66dd16f4f486d872685ededb406a96342a51ce
Matthew Denitz <23609571+mdenitz@users.noreply.github.com>
2023-04-04 16:47:13 -0700

    Currently not outputting to text file need to debug

--- yelpReviews/yelpReviews/spiders/yelpReviewSpider.py
+++ yelpReviews/yelpReviews/spiders/yelpReviewSpider.py
@@ -1,26 +1,48 @@
 import scrapy
+import re
 
 
 class yelpReviewSpider(scrapy.Spider):
+    """ FileObject contains file metadata and makes attempts to get mp3 genre with spotipy package. 
+
+    Class Attributes:
+        name (str): Name of the Crawler
+
+    Attributes:
+        start_urls (list): List of starting urls (contains only yelp biz review)
+        initial_domain (str): Starting initial yelp restaurant url
+        """
     name = 'yelpReview'
-    # Sort reviews by date
-    sort_desc = '&sort_by=date_desc'
-    start = '?start='
     # We need to collect url input from command line
     def __init__(self, *args, **kwargs):
         super(yelpReviewSpider,self).__init__(*args, **kwargs)
         self.start_urls = [kwargs.get('start_url')]
         self.initial_domain = self.start_urls[0]
     def parse(self, response):
+        """Responsible for building out all the restaraunt review urls
+        Args:
+            response (str): the response to parse
+        """
         # Get the number of reviews to traverse pages correctly
         # This is also important to ignore invalid reviews that appear on yelp
-        total_reviews = int(response.xpath('//html/body/yelp-react-root/div/div/div/div/div/div/div/div/span/a/text()').get().split()[0])
+        review_str = response.xpath('//html/body/yelp-react-root/div/div/div/div/div/div/div/div/span/a/text()').get()
+
+        total_reviews = int(re.findall(r'\d+', review_str)[0]) 
         # Traverse through urls and parse
-        for review_count in range(total_reviews, -10, -10):
-            new_url = self.initial_domain + self.start + str(review_count) + self.sort_desc
+        for review_count in range(0, total_reviews, 10):
+            new_url = "{initial}?sort_by=date_desc&start={count}".format(
+                    initial=self.initial_domain, count=str(review_count))
+            if review_count == 0:
+                new_url = "{initial}?sort_by=date_desc".format(
+                        initial=self.initial_domain)
             yield scrapy.Request(new_url, callback=self.parse_page)
 
     def parse_page(self, response):
+        """ Grabs relevant review data and yields into dictionary entry
+        Args:
+            response (str): the response to parse
+        """
+
         # Remove instances of <br> so reviews arent broken up
         response = response.replace(body=response.body.replace(b'<br>', b''))
         # Following will query relevant data
